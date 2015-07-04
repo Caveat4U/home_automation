869b8c10323cba4deb97e140a4c7d7c2591c7425
Chris Sterling <chris@techingyouout.com>
2015-07-04 14:17:33 -0600

    Working better.

--- __init__.py
+++ __init__.py
@@ -1,57 +1,51 @@
 from iclickerpoll import *
+from scenes import *
 import signal, argparse
+from os import geteuid
 
-parser = argparse.ArgumentParser(description='Start an iClicker poll')
-parser.add_argument('--debug', action='store_true', default=False,
-                    help='Display debug information about the USB transactions')
-parser.add_argument('--type', type=str, default='alpha',
-                    help='Sets the poll type to alpha, numeric, or alphanumeric')
-parser.add_argument('--duration', type=str, default='0m0s',
-                    help='Sets the duration of the poll in minutes and seconds. 0m0s is unlimited.')
-parser.add_argument('--dest', type=str, default='',
-                    help='Sets the file to save polling data to.')
-parser.add_argument('--frequency', type=str, default='aa',
-                    help='Sets the two base-station frequency codes. Should be formatted as two letters (e.g., \'aa\' or \'ab\')')
+if geteuid() != 0:
+    print "You must be root in order to use the home automation program."
+    exit(1)
 
-args = parser.parse_args()
+class HomeAutomation(object):
+    def __init__(self):
+        self.light_pool = None
+        self.initialize_iclicker()
+        self.initialize_lights()
+        self.freq1 = self.freq2 = 'a'
 
-#
-# Process all the arguments
-#
+    def initialize_lights(self):
+        p = PhueLights()
+        w = WemoLights()
+        self.light_pool = [p,w]
 
-if args.debug:
-    log.setLevel(0)
-if args.type in ['alpha', 'numeric', 'alphanumeric']:
-    poll_type = args.type
-else:
-    raise ValueError("Poll type must be 'alpha', 'numeric', or 'alphanumeric', not '{0}'".format(args.type))
-if args.duration:
-    poll_duration = 1000
-if args.frequency:
-    freq1 = args.frequency[0].lower()
-    freq2 = args.frequency[1].lower()
-    if freq1 not in ('a', 'b', 'c', 'd') or freq2 not in ('a', 'b', 'c', 'd'):
-        raise ValueError("Frequency combintation '{0}{1}' is not valid".format(freq1, freq2))
+        # Kick off the threads
+        [t.start() for t in self.light_pool]
+        return True
 
+    def initialize_iclicker(self):
+        # Initiate the polling
+        print 'Finding iClicker Base' 
+        base = IClickerBase()
+        base.get_base()
+        print 'Initializing iClicker Base'
+        base.initialize(self.freq1, self.freq2)
+        poll = IClickerPoll(base)
+        
+    def set_scene(scene_name = 'cool'):      
+        #[light_set.save_current_scene(scene_name) for light_set in light_pool]
+        [light_set.set_scene(scene_name) for light_set in light_pool]
 
-#
-# Initiate the polling
-#
-print('Finding iClicker Base')
-base = IClickerBase()
-base.get_base()
-print('Initializing iClicker Base')
-base.initialize(freq1, freq2)
+    def __del__():
+        # Wait for all threads to complete
+        for t in light_pool:
+            t.join()
 
-poll = IClickerPoll(base)
-signal.signal(signal.SIGINT, lambda *x: close_pole(poll))
-# Set a callback to stop the poll after the desired amount of time
-if poll_duration:
-    stop_timer = threading.Timer(poll_duration, lambda *x: close_pole(poll))
-    stop_timer.start()
-print('Poll Started')
-poll.start_poll(poll_type)
-
-# If we made it this far and stop_timer wasn't triggered, we were asked to stop another
-# way, so we should stop the stop_timer
-stop_timer.cancel()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Start your home up Chris...')
+    parser.add_argument('--debug', action='store_true', default=False,
+    args = parser.parse_args()
+    # Process all the arguments
+    #if args.debug:
+    #    log.setLevel(0)
+    my_home = HomeAutomation()
--- my_hue.py
+++ my_hue.py
@@ -3,43 +3,34 @@ from phue import Bridge
 from ouimeaux.environment import Environment
 from ouimeaux.signals import statechange, receiver
 import json
+from abc import ABCMeta,abstractmethod
+from os.path import exists as file_exists
+import threading
 
-class MyLights():
-    def __init__(self):
-        self.b = Bridge('192.168.1.2')
-        # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
-        #self.b.connect()
-        self.env = Environment()
-        self.env.start()
-        self.env.discover(5)
+class MyLights(threading.Thread):
+    __metaclass__ = ABCMeta
+    def __init__(self, light_type = ""):
+        threading.Thread.__init__(self)
+        self._light_type = ''
+        self.outfile = "%s_scenes.json"
+        self.light_type = light_type
 
+    @abstractmethod
+    def run(self):
+        pass
+
+    @property
+    def light_type(self):
+        return self._light_type
+
+    @light_type.setter
+    def light_type(self, value):
+        self.outfile = self.outfile % value
+        self._light_type = value
+
+    @abstractmethod
     def get_current_state(self):
-        # Get the bridge state (This returns the full dictionary that you can explore)
-        self.b.get_api()
-
-        current_state = {}
-
-        wemo_lights = self.env.list_switches()
-        for wemo_light in wemo_lights:
-            current_state[wemo_light] = {}
-            current_state[wemo_light]['type'] = 'wemo'
-            #current_state[wemo_light]['']
-            current_state[wemo_light]['state'] = self.env.get_switch(wemo_light).get_state()
-
-
-        for l in  self.b.lights:
-            name = l.name
-            current_state[name] = {}
-            current_state[name]['type'] = 'phue'
-            current_state[name]['brightness'] = l.brightness
-            current_state[name]['on'] = l.on
-            try:
-                current_state[name]['saturation'] = l.saturation
-                current_state[name]['hue'] = hex(l.hue)
-                current_state[name]['transitiontime'] = l.transitiontime
-            except KeyError,e:
-                pass # No key found
-        return current_state
+        pass
 
     def save_current_scene(self, scene_name):
         scenes = self.load_scenes()
@@ -48,11 +39,14 @@ class MyLights():
         # Save the current state into the dictionary
         scenes[scene_name] = current_scene
         # Write it all out
-        with open('scenes.json', 'w') as fp:
+        with open(self.outfile, 'w') as fp:
             json.dump(scenes, fp, sort_keys=True, indent=4, separators=(',', ': '))
 
     def load_scenes(self):
-        with open('scenes.json', 'r') as fp:
+        if not file_exists(self.outfile):
+            print "Could not find the '%s' file." % self.outfile
+            return {}
+        with open(self.outfile, 'r') as fp:
             if fp.read():
                 fp.seek(0)
                 scenes = json.load(fp)
@@ -67,9 +61,10 @@ class MyLights():
     def set_scene(self, scene_name='cool'):
         # Load the scene into active memory
         scene = self.load_scene(scene_name)
-
-        # Get the lights so I can interact with them by name
-        phue_lights = self.b.get_light_objects('name')
+        if self.light_type == 'phue':
+            # Get the lights so I can interact with them by name
+            phue_lights = self.b.get_light_objects('name')
+        
         for light_name, light in scene.items():
             if light['type'] == 'phue':
                 this_light = phue_lights[light_name]
@@ -87,9 +82,70 @@ class MyLights():
             else:
                 print "type undefined for %s" % light_name
 
+class WemoLights(MyLights):
+    def __init__(self):
+        super(WemoLights, self).__init__('wemo')
+        
+    def run(self):
+        self.env = Environment()
+        self.env.start()
+        self.env.discover(10)
+
+    def get_current_state(self):
+        current_state = {}
+        wemo_lights = self.env.list_switches()
+        if len(wemo_lights) == 0:
+            print "No wemo lights were discovered"
+        for wemo_light in wemo_lights:
+            current_state[wemo_light] = {}
+            current_state[wemo_light]['type'] = 'wemo'
+            current_state[wemo_light]['state'] = self.env.get_switch(wemo_light).get_state()
+        return current_state
+
+class PhueLights(MyLights):
+    def __init__(self):
+        super(PhueLights, self).__init__('phue')
+
+    def run(self):
+        self.b = Bridge('192.168.1.2')
+        # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
+        #self.b.connect()
+
+    def get_current_state(self):
+        # Get the bridge state (This returns the full dictionary that you can explore)
+        self.b.get_api()
+
+        current_state = {}
+
+        for l in  self.b.lights:
+            name = l.name
+            current_state[name] = {}
+            current_state[name]['type'] = 'phue'
+            current_state[name]['brightness'] = l.brightness
+            current_state[name]['on'] = l.on
+            try:
+                current_state[name]['saturation'] = l.saturation
+                current_state[name]['hue'] = hex(l.hue)
+                current_state[name]['transitiontime'] = l.transitiontime
+            except KeyError,e:
+                pass # No key found
+        return current_state
+
 
 if __name__ == '__main__':
-    ml = MyLights()
-    ml.set_scene('off')
-    #ml.save_current_scene('off')
+    p = PhueLights()
+    w = WemoLights()
+    light_pool = [p,w]
+
+    # Kick off the threads
+    [t.start() for t in light_pool]
+    
+
+    scene_name = 'cool'
+    #[light_set.save_current_scene(scene_name) for light_set in light_pool]
+    [light_set.set_scene(scene_name) for light_set in light_pool]
+
+    # Wait for all threads to complete
+    for t in light_pool:
+        t.join()
     
